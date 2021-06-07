d8aa45c41b7e1d0fc6da74e6166d89aa79b5a414
Johnatan Osterloh <johnatanosterloh@gmail.com>
2021-06-07 16:32:52 -0300

    Instancia dos objetos Pessoa e PessoaController

--- src/br/com/senai/pessoa/ProgramaPrincipal.java
+++ src/br/com/senai/pessoa/ProgramaPrincipal.java
@@ -1,10 +1,42 @@
 package br.com.senai.pessoa;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProgramaPrincipal {
 
 	public static void main(String[] args) {
+		List<Pessoa> pessoas = new ArrayList<>();
 
+		PessoaController pessoaController = new PessoaController();
+
+		boolean sair = false;
+
+		do{
+			pessoaController.menu();
+
+			int opcao = pessoaController.leOpcao();
+
+			switch(opcao){
+				case 1:
+					pessoas.add(pessoaController.cadastrarPessoa());
+					break;
+
+				case 2:
+					pessoaController.listarPessoas(pessoas);
+					break;
+
+				case 9:
+					sair = true;
+					break;
+
+				default:
+					System.out.println("Opção inválida!");
+					break;
+			}
+		}while(!sair);
+
+		System.out.println("Sistema finalizado!");
 	}
 
 }
